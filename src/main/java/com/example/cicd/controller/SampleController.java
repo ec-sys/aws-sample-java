7d902f0f1fc0632b1f32b17db1fb5f79b14162b9
longdangdjv <long.dang@drjoy.vn>
2022-05-07 15:40:45 +0000

    update

--- src/main/java/com/example/cicd/controller/SampleController.java
+++ src/main/java/com/example/cicd/controller/SampleController.java
@@ -5,8 +5,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SampleController {
+    @GetMapping("")
+    public String hello() {
+        return "hello you";
+    }
+
     @GetMapping("/test")
     public String test() {
-        return "hello you";
+        return "hello you test";
     }
 }
